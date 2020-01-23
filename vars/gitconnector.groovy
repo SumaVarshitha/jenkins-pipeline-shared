148133a5a0e542032b49a7236189372c7638879a
SumaVarshitha <55625932+SumaVarshitha@users.noreply.github.com>
2020-01-23 15:13:54 +0530

    Create gitconnector.groovy

---
+++ vars/gitconnector.groovy
@@ -0,0 +1,12 @@
+def call(){
+  httpRequest acceptType: 'APPLICATION_JSON', authentication: 'connectors', consoleLogResponseBody: true, contentType: 'APPLICATION_JSON', httpMode: 'POST', requestBody: '''{
+                "name": "karan",
+                  "description": "This is your first repository",
+                  "homepage": "https://github.com",
+                  "private": false,
+                  "has_issues": true,
+                  "has_projects": true,
+                  "has_wiki": true
+                }''', responseHandle: 'NONE', url: 'https://api.github.com/user/repos'
+}
+
