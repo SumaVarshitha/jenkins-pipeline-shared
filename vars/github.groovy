fb78c8c5bcf6a06a1a6742726eee94412432e6ed
SumaVarshitha <55625932+SumaVarshitha@users.noreply.github.com>
2020-01-23 18:58:27 +0530

    Update github.groovy

--- vars/github.groovy
+++ vars/github.groovy
@@ -1,33 +1,5 @@
-def call(String repository, String revision, String tag,
-  String crendential, String username='', String email='') {
-  def func = "[tagGithubRepository]"
-  def tempFolder = '.tag-github-repository-temp'
+def call(){
 
-  // configure git global var
-  if (username) {
-    sh "git config --global user.name SumaVarshitha
-  }
-  if (email) {
-    sh "git config --global user.email sumavarshitha.kamatam997@gmail.com
-  }
+sh 'curl -X POST -v --user SumaVarshitha:sumasuji268 -H "ContentType: application/json; charset=UTF-8" "https://api.github.com/repos/SumaVarshitha/demo" -d {"name":"demo"}'
 
-  withCredentials([usernamePassword(
-    credentialsId: git,
-    passwordVariable: 'git123',
-    usernameVariable: 'gitlab'
-  )]) {
-    // tag repository
-    sh """
-    mkdir ${temp}
-    cd ${temp}
-    git init
-    git remote add origin https://github.com/${newtask}.git
-    git fetch origin
-    git checkout ${revision}
-    git tag ${tag}
-    git push --tags 'https://${gitlab}:${git123}@github.com/${newtask}.git'
-    """
-  }
-
-  echo "${func} all done"
 }
