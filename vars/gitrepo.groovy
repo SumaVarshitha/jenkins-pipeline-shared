1a1d40ea803ebeef39130a37445cb5dabd639a41
SumaVarshitha <55625932+SumaVarshitha@users.noreply.github.com>
2020-01-23 19:31:56 +0530

    Update gitrepo.groovy

--- vars/gitrepo.groovy
+++ vars/gitrepo.groovy
@@ -1,3 +1,3 @@
 def call(){
-sh 'curl -u SumaVarshitha https://api.github.com/users/repos -d '{"name": "reponame"}'
+sh 'curl -u SumaVarshitha https://api.github.com/users/repos -d '{"name": "reponame"}''
 }
