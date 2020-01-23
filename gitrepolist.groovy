c991a4b74270cb144da6d59d75222dea6cf7e530
SumaVarshitha <55625932+SumaVarshitha@users.noreply.github.com>
2020-01-23 18:22:33 +0530

    Create gitrepolist.groovy

---
+++ gitrepolist.groovy
@@ -0,0 +1,6 @@
+def call(){
+
+sh 'curl -v --user SumaVarshitha:sumavarshitha.kamatam997@gmail.com -H "ContentType: application/json; charset=UTF-8"  https://api.bitbucket.org/2.0/repositories/Megalai'
+
+}
+© 2020 GitHub, Inc.
