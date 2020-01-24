ca1d33afecc686bcbf32008a5197784fabde999d
SumaVarshitha <55625932+SumaVarshitha@users.noreply.github.com>
2020-01-24 18:34:19 +0530

    Update sonarConnector.groovy

--- vars/sonarConnector.groovy
+++ vars/sonarConnector.groovy
@@ -1,12 +1,12 @@
 def call()
 {
 //Creating Project in Sonar
-sh 'curl -u admin:admin -X POST "http://ec2-3-16-33-107.us-east-2.compute.amazonaws.com:9000/api/projects/create?key=newKey&name=EDN250"'
+sh 'curl -u admin:admin -X POST "http://ec2-3-16-33-107.us-east-2.compute.amazonaws.com:9000/api/projects/create?key=rigKey&name=EDN250"'
 //Quality Gate
 sh 'curl -u admin:admin -X POST "http://3.16.33.107:9000/api/qualitygates/create?name=EDN250"'
 sh 'curl -X GET http://3.16.33.107:9000/api/qualitygates/list'
-sh 'curl -u admin:admin -X POST "http://3.16.33.107:9000/api/qualitygates/create_condition?gateId=11&metric=blocker_violations&op=GT&warning=5&error=10"'
-sh 'curl -u admin:admin -X POST "http://3.16.33.107:9000/api/qualitygates/create_condition?gateId=11&metric=critical_violations&op=GT&warning=5&error=10"'
-sh 'curl -u admin:admin -X POST "http://3.16.33.107:9000/api/qualitygates/select?gateId=11&projectId=1"'
-sh 'curl -u admin:admin -X POST "http://3.16.33.107:9000/api/qualitygates/select_as_default?id=1"'
+sh 'curl -u admin:admin -X POST "http://3.16.33.107:9000/api/qualitygates/create_condition?gateId=12&metric=blocker_violations&op=GT&warning=5&error=10"'
+sh 'curl -u admin:admin -X POST "http://3.16.33.107:9000/api/qualitygates/create_condition?gateId=12&metric=critical_violations&op=GT&warning=5&error=10"'
+sh 'curl -u admin:admin -X POST "http://3.16.33.107:9000/api/qualitygates/select?gateId=12&projectId=1"'
+//sh 'curl -u admin:admin -X POST "http://3.16.33.107:9000/api/qualitygates/select_as_default?id=1"'
 }
