77f92224159311275947a310ec7f0b3cecc87573
SumaVarshitha <55625932+SumaVarshitha@users.noreply.github.com>
2020-01-24 09:35:11 +0530

    Update gitrepo.groovy

--- vars/gitrepo.groovy
+++ vars/gitrepo.groovy
@@ -1,3 +1,3 @@
 def call(){
-sh 'curl -H  "Authorization:token <ea519893046425a0f31cf5254f6b1017159fc9e8>" "ContentType: application/json; charset=UTF-8" "https://api.github.com/user//repo6" --data {"name":"repo6"}'
+sh 'curl -H  "Authorization:token <ea519893046425a0f31cf5254f6b1017159fc9e8>" "ContentType: application/json;  --data '{"name":"repo6"}' charset=UTF-8" https://api.github.com/user/repos'
 }
