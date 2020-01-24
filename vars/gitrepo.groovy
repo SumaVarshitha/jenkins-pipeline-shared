aac41558c3f314d9678d7da894c5e16ea6fb2b67
SumaVarshitha <55625932+SumaVarshitha@users.noreply.github.com>
2020-01-24 12:17:38 +0530

    Update gitrepo.groovy

--- vars/gitrepo.groovy
+++ vars/gitrepo.groovy
@@ -1,14 +1,4 @@
 def call(){
-sh 'curl -X POST -v --user SumaVarshitha:sumasuji268 -H "ContentType: application/json; charset=UTF-8" requestBody:'body''{
-                "name": "varshi",
-                  "description": "This is your first repository",
-                  "homepage": "https://github.com",
-                  "private": false,
-                  "has_issues": true,
-                  "has_projects": true,
-                  "has_wiki": true
-                }'''
-
-
- "https://api.github.com/user/repos"'
+sh 'curl -H "Authorization: token <626cc053030fb8ccac268f6541c258ee5752e9ed>" --data '{"name":"<varshi>"}' https://api.github.com/user/repos
+'
 }
