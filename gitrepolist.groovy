51db823100e775d0623fad413cf01e3d71c18b6e
SumaVarshitha <55625932+SumaVarshitha@users.noreply.github.com>
2020-01-24 15:18:48 +0530

    Delete gitrepolist.groovy

--- gitrepolist.groovy
+++
@@ -1,6 +0,0 @@
-def call(){
-
-sh 'curl -v --user SumaVarshitha:sumavarshitha.kamatam997@gmail.com -H "ContentType: application/json; charset=UTF-8"  https://api.bitbucket.org/2.0/repositories/Megalai'
-
-}
-© 2020 GitHub, Inc.
