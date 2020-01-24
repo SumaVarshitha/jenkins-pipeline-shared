1e94ebc69db81a0b5116f6a12b908d0d214272b6
SumaVarshitha <55625932+SumaVarshitha@users.noreply.github.com>
2020-01-24 18:35:05 +0530

    Update gitlabrepo.groovy

--- vars/gitlabrepo.groovy
+++ vars/gitlabrepo.groovy
@@ -9,7 +9,7 @@ sh '''
   -H 'content-type: application/json' \
   -H 'postman-token: 284851de-59b0-f9e2-af1f-823acf4a7be8' \
   -d ' {
-                "name": "bhavya",
+                "name": "bhavi",
                   "description": "This is your first project",
                   "homepage": "https://gitlab.com",
                   "private": false,
