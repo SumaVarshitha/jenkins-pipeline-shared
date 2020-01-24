2452b8a582ff42c08c65bb324f79b99ad1ee9b74
SumaVarshitha <55625932+SumaVarshitha@users.noreply.github.com>
2020-01-24 16:14:40 +0530

    Update gitlabrepo.groovy

--- vars/gitlabrepo.groovy
+++ vars/gitlabrepo.groovy
@@ -1,8 +1,19 @@
 def call()
 {
-curl -X POST "http://gitlab.example.com/api/v4/projects" \
-     -H "PRIVATE-TOKEN: ravzJJcJ6ih-yrpchiLs" \
-     -H "Content-Type: application/json; charset=utf-8" \
-     -d "{\"name\":\"foo-${RANDOM}\",\"issues_enabled\":\"true\",\"merge_requests_enabled\":\"true\",\"builds_enabled\":\"true\",\"wiki_enabled\":\"true\",\"snippets_enabled\":\"true\",\"shared_runners_enabled\":\"true\",\"public\":\"true\",\"visibility_level\":0,\"public_builds\":\"true\",\"only_allow_merge_if_build_succeeds\":\"true\",\"lfs_enabled\":\"true\",\"request_access_enabled\":\"true\"}" \
-| jq . | grep enabled
+curl -X POST \
+  'https://gitlab.com/api/v4/projects?private_token=%0AravzJJcJ6ih-yrpchiLs' \
+  -H 'accept: application/json' \
+  -H 'authorization: Basic c3VtYXZhcnNoaXRoYS5rYW1hdGFtOTk3QGdtYWlsLmNvbTpyYXZ6SkpjSjZpaC15cnBjaGlMcw==' \
+  -H 'cache-control: no-cache' \
+  -H 'content-type: application/json' \
+  -H 'postman-token: 284851de-59b0-f9e2-af1f-823acf4a7be8' \
+  -d ' {
+                "name": "suma123",
+                  "description": "This is your first project",
+                  "homepage": "https://gitlab.com",
+                  "private": false,
+                  "has_issues": true,
+                  "has_projects": true,
+                  "has_wiki": true
+                }'
 }
