8822c521436b73f9c6bae5a06c584b76cacdf703
SumaVarshitha <55625932+SumaVarshitha@users.noreply.github.com>
2020-01-24 16:41:52 +0530

    Update gitlabrepo.groovy

--- vars/gitlabrepo.groovy
+++ vars/gitlabrepo.groovy
@@ -2,7 +2,7 @@ def call()
 {
 sh '''
   curl -X POST \
- 'https://gitlab.com/api/v4/projects?private_token=%0AravzJJcJ6ih-yrpchiLs' \
+ 'https://gitlab.com/api/v4/projects?private_token=ravzJJcJ6ih-yrpchiLs' \
   -H 'accept: application/json' \
   -H 'authorization: Basic c3VtYXZhcnNoaXRoYS5rYW1hdGFtOTk3QGdtYWlsLmNvbTpyYXZ6SkpjSjZpaC15cnBjaGlMcw==' \
   -H 'cache-control: no-cache' \
