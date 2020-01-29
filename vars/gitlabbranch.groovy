3b63f82bdee48a24bf1012af169c69a9504cea83
SumaVarshitha <55625932+SumaVarshitha@users.noreply.github.com>
2020-01-29 13:03:37 +0530

    Update and rename gitlabbranch.groovy to gitlabbranchlist.groovy

--- vars/gitlabbranch.groovy
+++
@@ -1,4 +0,0 @@
-def call()
-{
-sh 'curl --header "PRIVATE-TOKEN: <ravzJJcJ6ih-yrpchiLs>" https://gitlab.example.com/api/v4/projects/5/repository/branches'
-}
---
+++ vars/gitlabbranchlist.groovy
@@ -0,0 +1,4 @@
+def call()
+{
+sh 'curl --header "PRIVATE-TOKEN: <ravzJJcJ6ih-yrpchiLs>" https://gitlab.com/api/v4/projects/16500283/repository/branches'
+}
