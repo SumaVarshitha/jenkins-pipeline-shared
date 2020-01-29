588814830e31b67f5c3574ec7fbe1fe77395cf54
SumaVarshitha <55625932+SumaVarshitha@users.noreply.github.com>
2020-01-29 16:13:38 +0530

    Update gitbranchlist.groovy

--- vars/gitbranchlist.groovy
+++ vars/gitbranchlist.groovy
@@ -3,6 +3,5 @@ sh '''
 curl -X GET \
   https://api.github.com/repos/SumaVarshitha/jenkins-pipeline-shared/branches \
   -H 'cache-control: no-cache' \
-  -H 'postman-token: 2e6c08e1-f5db-81c6-6c41-9cdee376cea4'
   '''
 }
