046f292ef1ffb50ea79918560a2920913de5ff35
SumaVarshitha <55625932+SumaVarshitha@users.noreply.github.com>
2020-01-29 12:42:12 +0530

    Update gitlabbranch.groovy

--- vars/gitlabbranch.groovy
+++ vars/gitlabbranch.groovy
@@ -1,4 +1,4 @@
 def call()
 {
-sh 'curl --header "PRIVATE-TOKEN: <your_access_token>" https://gitlab.example.com/api/v4/projects/5/repository/branches'
+sh 'curl --header "PRIVATE-TOKEN: <ravzJJcJ6ih-yrpchiLs>" https://gitlab.example.com/api/v4/projects/5/repository/branches'
 }
