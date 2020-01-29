86cb9b177d7e4c4dbb1dce47da1c9f22ec8c6c45
SumaVarshitha <55625932+SumaVarshitha@users.noreply.github.com>
2020-01-29 13:09:53 +0530

    Update gitlabbranchlist.groovy

--- vars/gitlabbranchlist.groovy
+++ vars/gitlabbranchlist.groovy
@@ -1,8 +1,10 @@
 def call()
 {
-sh 'curl -X GET \
+sh '''
+  curl -X GET \
   https://gitlab.com/api/v4/projects/16500283/repository/branches \
   -H 'authorization: Basic c3VtYXZhcnNoaXRoYS5rYW1hdGFtOTk3QGdtYWlsLmNvbTpzdW1hc3VqaTI2OA==' \
   -H 'cache-control: no-cache' \
-  -H 'postman-token: 1278cddd-133d-4a82-5588-c065664a4ad7''
+  -H 'postman-token: 1278cddd-133d-4a82-5588-c065664a4ad7'
+  '''
 }
