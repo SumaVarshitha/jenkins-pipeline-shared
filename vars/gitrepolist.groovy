d4a0040dd6f4f3e3e6406e3db454fe5fb1199e7d
SumaVarshitha <55625932+SumaVarshitha@users.noreply.github.com>
2020-01-29 14:39:46 +0530

    Update gitrepolist.groovy

--- vars/gitrepolist.groovy
+++ vars/gitrepolist.groovy
@@ -1,5 +1,6 @@
 def call(){
-def name = System.console().readLine 'What is your name? '
+print "What is your name? "
+def name = System.in.newReader().readLine()
 println "Your name is " + name
 sh 'curl -v --user SumaVarshitha:sumasuji268 -H "ContentType: application/json; charset=UTF-8"  https://api.github.com/users/SumaVarshitha/repos'
 
