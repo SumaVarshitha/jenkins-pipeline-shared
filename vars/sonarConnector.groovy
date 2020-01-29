316fcea875ab711852f7b55654cbee69554a4dc1
SumaVarshitha <55625932+SumaVarshitha@users.noreply.github.com>
2020-01-29 17:49:30 +0530

    Update sonarConnector.groovy

--- vars/sonarConnector.groovy
+++ vars/sonarConnector.groovy
@@ -3,7 +3,8 @@ def call(ProjectKey,ProjectName,QualityGateName,GateId,ProjectId)
   echo "$ProjectName"
 //Creating Project in Sonar
 sh 'curl -u admin:admin -X POST "http://ec2-3-16-33-107.us-east-2.compute.amazonaws.com:9000/api/projects/create?key=${ProjectKey}&name=${ProjectName}"'
-//Quality Gate
+echo "$ProjectName"
+  //Quality Gate
 sh 'curl -u admin:admin -X POST "http://3.16.33.107:9000/api/qualitygates/create?name=${QualityGateName}"'
 sh 'curl -X GET http://3.16.33.107:9000/api/qualitygates/list'
 sh 'curl -u admin:admin -X POST "http://3.16.33.107:9000/api/qualitygates/create_condition?gateId=${GateId}&metric=blocker_violations&op=GT&warning=5&error=10"'
