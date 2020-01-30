25eaeb30880cd9f933794d98eca85f339c41a93d
SumaVarshitha <55625932+SumaVarshitha@users.noreply.github.com>
2020-01-30 18:22:38 +0530

    Update sonarConnector.groovy

--- vars/sonarConnector.groovy
+++ vars/sonarConnector.groovy
@@ -11,7 +11,7 @@ def call(ProjectKey,ProjectName,QualityGateName,GateId,ProjectId)
   //Quality Gate
   sh "curl -u admin:admin -X POST http://3.16.33.107:9000/api/qualitygates/create?name=${QualityGateName}"
   sh "curl -X GET http://3.16.33.107:9000/api/qualitygates/list"
-  sh "curl -u admin:admin -X POST http://3.16.33.107:9000/api/qualitygates/create_condition?gateId=14&metric=blocker_violations&op=GT&warning=5&error=10"
+  sh "curl -u admin:admin -X POST "http://3.16.33.107:9000/api/qualitygates/create_condition?gateId=14&metric=blocker_violations&op=GT&warning=5&error=10""
   sh "curl -u admin:admin -X POST http://3.16.33.107:9000/api/qualitygates/create_condition?gateId=$GateId&metric=critical_violations&op=GT&warning=5&error=10"
   sh "curl -u admin:admin -X POST http://3.16.33.107:9000/api/qualitygates/select?gateId=$GateId&projectId=$ProjectId"
 //sh 'curl -u admin:admin -X POST "http://3.16.33.107:9000/api/qualitygates/select_as_default?id=1"'
