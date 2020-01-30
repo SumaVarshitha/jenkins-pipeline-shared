8563da7073115806c450f6c0d7eab55ea966b630
SumaVarshitha <55625932+SumaVarshitha@users.noreply.github.com>
2020-01-30 15:32:50 +0530

    Update gitrepolist.groovy

--- vars/gitrepolist.groovy
+++ vars/gitrepolist.groovy
@@ -1,5 +1,21 @@
+import groovy.json.JsonSlurper 
+
+@NonCPS
+createRepo(String data){
+def jsonSlurper = new JsonSlurper() 
+def resultJson = jsonSlurper.parseText(data)
+def projUrl = resultJson.url
+//def projUrl = resultJson.url
+httpRequest authentication: 'github', contentType: 'APPLICATION_JSON', customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json']], httpMode: 'GET', requestBody: """
+{
+}""", responseHandle: 'NONE', url: "${projUrl}"
+}
 def call(){
+def request = libraryResource 'githubcollector.json'
+createRepo(request)
+}
+/*def call(){
   
 sh 'curl -v --user SumaVarshitha:sumasuji268 -H "ContentType: application/json; charset=UTF-8"  https://api.github.com/users/SumaVarshitha/repos'
 
-}
+}*/
