80c393ef6fcb2ef48cb3d6a9e3d58761602d3448
SumaVarshitha <55625932+SumaVarshitha@users.noreply.github.com>
2020-01-30 12:39:21 +0530

    Create createrepogitlab.groovy

---
+++ vars/createrepogitlab.groovy
@@ -0,0 +1,20 @@
+import groovy.json.JsonSlurper 
+
+@NonCPS
+createRepo(String data){
+def jsonSlurper = new JsonSlurper() 
+def resultJson = jsonSlurper.parseText(data)
+def repoName = resultJson.name
+def projUrl = resultJson.url
+//def projUrl = resultJson.url
+httpRequest authentication: 'git', contentType: 'APPLICATION_JSON', customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json']], httpMode: 'POST', requestBody: """
+{
+    "name": "${repoName}",
+    "scmId": "git",
+    "forkable": true
+}""", responseHandle: 'NONE', url: "${projUrl}"
+}
+def call(){
+def request = libraryResource 'data.json'
+createrepogitlab(request)
+}
