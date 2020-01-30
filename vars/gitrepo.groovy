b9b0515722875ac34c4a9159a6f7ba33c1ea6bc2
SumaVarshitha <55625932+SumaVarshitha@users.noreply.github.com>
2020-01-30 15:17:47 +0530

    Update gitrepo.groovy

--- vars/gitrepo.groovy
+++ vars/gitrepo.groovy
@@ -5,25 +5,14 @@ createRepo(String data){
 def jsonSlurper = new JsonSlurper() 
 def resultJson = jsonSlurper.parseText(data)
 def repoName = resultJson.name
+def projUrl = resultJson.url
 //def projUrl = resultJson.url
-//def projUrl = resultJson.url
- sh '''
-curl -X POST \
-  https://api.github.com/user/repos \
-  -H 'accept: application/json' \
-  -H 'authorization: Basic c3VtYXZhcnNoaXRoYS5rYW1hdGFtOTk3QGdtYWlsLmNvbTpzdW1hc3VqaTI2OA==' \
-  -H 'cache-control: no-cache' \
-  -H 'postman-token: 5d076930-9214-41a9-3383-0f081485a47a' \
-  -d ' {
-                "name": "${repoName}",
-                  "description": "This is your first repository",
-                  "homepage": "https://github.com",
-                  "private": false,
-                  "has_issues": true,
-                  "has_projects": true,
-                  "has_wiki": true
-                }'
-                '''
+ httpRequest authentication: 'github', contentType: 'APPLICATION_JSON', customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json']], httpMode: 'POST', requestBody: """
+{
+    "name": "${repoName}",
+    "scmId": "git",
+    "forkable": true
+}""", responseHandle: 'NONE', url: "${projUrl}"
 }
 def call(){
 def request = libraryResource 'data.json'
