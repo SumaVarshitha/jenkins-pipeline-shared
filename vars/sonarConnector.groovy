c62bcd11855a0e057f8338c9a75c6614d624b585
SumaVarshitha <55625932+SumaVarshitha@users.noreply.github.com>
2020-01-30 17:58:20 +0530

    Update sonarConnector.groovy

--- vars/sonarConnector.groovy
+++ vars/sonarConnector.groovy
@@ -1,16 +1,16 @@
-def call(ProjectKey,ProjectName,QualityGateName,GateId,ProjectId)
+def call(ProjectKey,ProjectName,QualityGateName,x,y)
 {
   echo "$ProjectName"
-  int x=Integer.parseInt("GateId");
-  int y=Integer.parseInt("ProjectId");
+  //int x=Integer.parseInt("GateId");
+  //int y=Integer.parseInt("ProjectId");
 //Creating Project in Sonar
   sh "curl -u admin:admin -X POST http://ec2-3-16-33-107.us-east-2.compute.amazonaws.com:9000/api/projects/create?key=${ProjectKey}&name=${ProjectName}"
   echo "$ProjectName"
   //Quality Gate
   sh "curl -u admin:admin -X POST http://3.16.33.107:9000/api/qualitygates/create?name=${QualityGateName}"
   sh "curl -X GET http://3.16.33.107:9000/api/qualitygates/list"
-  sh "curl -u admin:admin -X POST http://3.16.33.107:9000/api/qualitygates/create_condition?gateId=x&metric=blocker_violations&op=GT&warning=5&error=10"
-  sh "curl -u admin:admin -X POST http://3.16.33.107:9000/api/qualitygates/create_condition?gateId=x&metric=critical_violations&op=GT&warning=5&error=10"
-  sh "curl -u admin:admin -X POST http://3.16.33.107:9000/api/qualitygates/select?gateId=x&projectId=y"
+  sh "curl -u admin:admin -X POST http://3.16.33.107:9000/api/qualitygates/create_condition?gateId=$x&metric=blocker_violations&op=GT&warning=5&error=10"
+  sh "curl -u admin:admin -X POST http://3.16.33.107:9000/api/qualitygates/create_condition?gateId=$x&metric=critical_violations&op=GT&warning=5&error=10"
+  sh "curl -u admin:admin -X POST http://3.16.33.107:9000/api/qualitygates/select?gateId=$x&projectId=$y"
 //sh 'curl -u admin:admin -X POST "http://3.16.33.107:9000/api/qualitygates/select_as_default?id=1"'
 }
