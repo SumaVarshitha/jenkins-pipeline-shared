319701ff651e9f9328519a04b5a25cf4280e2db3
SumaVarshitha <55625932+SumaVarshitha@users.noreply.github.com>
2020-01-31 15:48:58 +0530

    Create log_github.groovy

---
+++ vars/log_github.groovy
@@ -0,0 +1,13 @@
+import groovy.json.JsonSlurper 
+@NonCPS
+
+def call(message)
+{
+ println(message)
+ def request = libraryResource 'data.json'
+ def jsonSlurper = new JsonSlurper() 
+ def resultJson = jsonSlurper.parseText(request)
+ def projectName = resultJson.gname
+  Date date = new Date() 
+  sh " echo '${date}' githubrepo with the reponame '${projectName}' ${message} >>log.txt"
+}
