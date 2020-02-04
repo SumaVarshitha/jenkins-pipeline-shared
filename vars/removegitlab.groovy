b63c9cca27a000643528da076431348c573c381f
SumaVarshitha <55625932+SumaVarshitha@users.noreply.github.com>
2020-02-04 18:02:26 +0530

    Create removegitlab.groovy

---
+++ vars/removegitlab.groovy
@@ -0,0 +1,11 @@
+def call()
+{
+sh '''
+curl -X DELETE \
+  'https://gitlab.com/api/v4/projects/16674665?%0Aprivate_token=VkjgB4Jdbaswh7FNXeC-' \
+  -H 'accept: application/json' \
+  -H 'authorization: Basic c3VtYXZhcnNoaXRoYS5rYW1hdGFtOTk3QGdtYWlsLmNvbTpWa2pnQjRKZGJhc3doN0ZOWGVDLQ==' \
+  -H 'cache-control: no-cache' \
+  -H 'postman-token: 75bf7f0c-b238-8ced-9af4-9ec6cc4eb8aa'
+  '''
+  }
