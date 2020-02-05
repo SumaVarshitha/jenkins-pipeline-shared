e1d131fea7731f05850317163b1270a285b6478f
SumaVarshitha <55625932+SumaVarshitha@users.noreply.github.com>
2020-02-05 16:44:19 +0530

    Update gitbranchlist.groovy

--- vars/gitbranchlist.groovy
+++ vars/gitbranchlist.groovy
@@ -1,7 +1,37 @@
-def call(){
+/*def call(){
 sh '''
 curl -X GET \
   https://api.github.com/repos/SumaVarshitha/jenkins-pipeline-shared/branches \
   -H 'cache-control: no-cache' \
   '''
+}
+httpRequest authentication: 'github', contentType: 'APPLICATION_JSON', customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json']], httpMode: 'POST', requestBody: """
+{
+    "name": "${repoName}",
+    "scmId": "git",
+    "forkable": true
+}""", responseHandle: 'NONE', url: "https://api.github.com/user/repos"
+*/
+def call(jsondata){
+ def jsonString = jsondata
+println(jsonString)
+def jsonObj = readJSON text: jsonString
+println(jsonObj.scm)
+
+//String a=jsonObj.alm.projects.project.project_name
+String a=jsonObj.scm.repositories.repository[1].name
+String repoName=a.replaceAll("\\[", "").replaceAll("\\]","");
+
+env.name = repoName
+
+
+//sh "rm -rf Text.xml"
+ println(a)
+ println(repoName)
+sh '''
+curl -X GET \
+  https://api.github.com/repos/SumaVarshitha/${repoName}/branches \
+  -H 'cache-control: no-cache' \
+  '''
+ 
 }
