95dbd205231f9b0e651284975f9b4e0bb7e0a121
SumaVarshitha <55625932+SumaVarshitha@users.noreply.github.com>
2020-02-05 13:05:15 +0530

    Update gitbranch.groovy

--- vars/gitbranch.groovy
+++ vars/gitbranch.groovy
@@ -39,14 +39,14 @@ String branchName=b.replaceAll("\\[", "").replaceAll("\\]","");
 
  sh '''
     curl -X POST \
-  https://api.github.com/repos/SumaVarshitha/repoName/git/refs \
+  https://api.github.com/repos/SumaVarshitha/${repoName}/git/refs \
   -H 'accept: application/json' \
   -H 'authorization: Basic c3VtYXZhcnNoaXRoYS5rYW1hdGFtOTk3QGdtYWlsLmNvbTpzdW1hc3VqaTI2OA==' \
   -H 'cache-control: no-cache' \
   -H 'content-type: application/json' \
   -H 'postman-token: 8aee7eca-0e7f-361c-6be6-9f3bfbd1f0df' \
   -d '{
-   "ref": "refs/heads/branchName",
+   "ref": "refs/heads/${branchName}",
    
   "sha": "8233111fce0f7fabab8753406eaf72d61b15a7ae"
 
