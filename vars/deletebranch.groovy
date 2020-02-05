9a0408139592d7e50a72e500b8f54d63dc983dcd
SumaVarshitha <55625932+SumaVarshitha@users.noreply.github.com>
2020-02-05 17:34:17 +0530

    Update deletebranch.groovy

--- vars/deletebranch.groovy
+++ vars/deletebranch.groovy
@@ -1,4 +1,4 @@
-def call()
+/*def call()
 {
 sh '''
 curl -X DELETE \
@@ -7,4 +7,32 @@ curl -X DELETE \
   -H 'cache-control: no-cache' \
   -H 'postman-token: c37c3e32-c8fc-3ee9-0a79-ab9a8f9f3904'
   '''
-  }
+  }*/
+
+def call(jsondata){
+ def jsonString = jsondata
+println(jsonString)
+def jsonObj = readJSON text: jsonString
+println(jsonObj.scm)
+
+//String a=jsonObj.alm.projects.project.project_name
+String a=jsonObj.scm.repositories.repository[1].name
+String repoName=a.replaceAll("\\[", "").replaceAll("\\]","");
+String b=jsonObj.scm.repositories.repository[1].branches.branch[0].name
+String branchName=b.replaceAll("\\[", "").replaceAll("\\]","")
+
+
+
+//sh "rm -rf Text.xml"
+ println(a)
+ println(repoName)
+   println(branchName)
+sh """
+curl -X DELETE \
+  https://api.github.com/repos/SumaVarshitha/${repoName}/git/refs/heads/${branchName} \
+  -H 'authorization: Basic c3VtYXZhcnNoaXRoYS5rYW1hdGFtOTk3QGdtYWlsLmNvbTpzdW1hc3VqaTI2OA==' \
+  -H 'cache-control: no-cache' \
+  -H 'postman-token: c37c3e32-c8fc-3ee9-0a79-ab9a8f9f3904'
+  """
+ 
+}
