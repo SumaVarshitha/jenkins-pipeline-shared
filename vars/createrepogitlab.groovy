b2709c2642f78b3272bace34484c041e8d3a609d
SumaVarshitha <55625932+SumaVarshitha@users.noreply.github.com>
2020-02-05 19:27:46 +0530

    Update createrepogitlab.groovy

--- vars/createrepogitlab.groovy
+++ vars/createrepogitlab.groovy
@@ -1,4 +1,4 @@
-import groovy.json.JsonSlurper 
+/*import groovy.json.JsonSlurper 
 
 @NonCPS
 createRepo(String data){
@@ -17,4 +17,29 @@ httpRequest authentication: 'git', contentType: 'APPLICATION_JSON', customHeader
 def call(){
 def request = libraryResource 'data.json'
 createRepo(request)
+}*/
+def call(jsondata){
+ def jsonString = jsondata
+println(jsonString)
+def jsonObj = readJSON text: jsonString
+println(jsonObj.scm)
+
+//String a=jsonObj.alm.projects.project.project_name
+String a=jsonObj.scm.projects.project[0].proj_name
+String projName=a.replaceAll("\\[", "").replaceAll("\\]","");
+
+
+
+
+//sh "rm -rf Text.xml"
+ println(a)
+ println(projName)
+
+ httpRequest authentication: 'git', contentType: 'APPLICATION_JSON', customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json']], httpMode: 'POST', requestBody: """
+{
+    "name": "${projName}",
+    "scmId": "git",
+    "forkable": true
+}""", responseHandle: 'NONE', url: "https://gitlab.com/api/v4/projects?private_token=VkjgB4Jdbaswh7FNXeC-"
 }
+ 
