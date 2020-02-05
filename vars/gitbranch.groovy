4b2b8599796a3f81411104226df94d9c66e99038
SumaVarshitha <55625932+SumaVarshitha@users.noreply.github.com>
2020-02-05 12:58:31 +0530

    Update gitbranch.groovy

--- vars/gitbranch.groovy
+++ vars/gitbranch.groovy
@@ -1,4 +1,4 @@
-def call()
+/*def call()
 {
     sh '''
     curl -X POST \
@@ -16,4 +16,42 @@ def call()
    
  }'
  '''
+}*/
+
+def call(jsondata){
+ def jsonString = jsondata
+println(jsonString)
+def jsonObj = readJSON text: jsonString
+println(jsonObj.scm)
+
+//String a=jsonObj.alm.projects.project.project_name
+String a=jsonObj.scm.repositories.repository.repo_name
+String repoName=a.replaceAll("\\[", "").replaceAll("\\]","");
+    
+String b=jsonObj.scm.repositories.repository.branches.branch.name
+String branchName=b.replaceAll("\\[", "").replaceAll("\\]","");
+
+
+
+//sh "rm -rf Text.xml"
+ println(a)
+ println(repoName)
+
+ sh '''
+    curl -X POST \
+  https://api.github.com/repos/SumaVarshitha/repoName/git/refs \
+  -H 'accept: application/json' \
+  -H 'authorization: Basic c3VtYXZhcnNoaXRoYS5rYW1hdGFtOTk3QGdtYWlsLmNvbTpzdW1hc3VqaTI2OA==' \
+  -H 'cache-control: no-cache' \
+  -H 'content-type: application/json' \
+  -H 'postman-token: 8aee7eca-0e7f-361c-6be6-9f3bfbd1f0df' \
+  -d '{
+   "ref": "refs/heads/branchName",
+   
+  "sha": "8233111fce0f7fabab8753406eaf72d61b15a7ae"
+
+   
+ }'
+ '''
 }
+ 
