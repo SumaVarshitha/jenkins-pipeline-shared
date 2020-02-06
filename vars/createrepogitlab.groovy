89c52f1c3be8d69029da24a7b51bd14f0cfe5980
SumaVarshitha <55625932+SumaVarshitha@users.noreply.github.com>
2020-02-06 12:55:01 +0530

    Update createrepogitlab.groovy

--- vars/createrepogitlab.groovy
+++ vars/createrepogitlab.groovy
@@ -35,11 +35,29 @@ String projName=a.replaceAll("\\[", "").replaceAll("\\]","");
  println(a)
  println(projName)
 
- httpRequest authentication: 'git', contentType: 'APPLICATION_JSON', customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json']], httpMode: 'POST', requestBody: """
+ /*httpRequest authentication: 'git', contentType: 'APPLICATION_JSON', customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json']], httpMode: 'POST', requestBody: """
 {
     "name": "${projName}",
     "scmId": "git",
     "forkable": true
 }""", responseHandle: 'NONE', url: "https://gitlab.com/api/v4/projects?private_token=VkjgB4Jdbaswh7FNXeC-"
+}*/
+ sh  """ curl -X POST \
+  'https://gitlab.com/api/v4/projects?private_token=VkjgB4Jdbaswh7FNXeC-' \
+  -H 'accept: application/json' \
+  -H 'authorization: Basic c3VtYXZhcnNoaXRoYS5rYW1hdGFtOTk3QGdtYWlsLmNvbTpyYXZ6SkpjSjZpaC15cnBjaGlMcw==' \
+  -H 'cache-control: no-cache' \
+  -H 'content-type: application/json' \
+  -H 'postman-token: a923a67e-9e1d-b260-e5ee-b0d55ac40009' \
+  -d ' {
+                "name": "${projName}",
+                  "description": "This is your first project",
+                  "homepage": "https://gitlab.com",
+                  "private": false,
+                  "has_issues": true,
+                  "has_projects": true,
+                  "has_wiki": true
+                }' -o projectid.json
+                """
 }
  
