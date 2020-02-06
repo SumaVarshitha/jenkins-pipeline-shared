13370ffdbf4078e1cc57f22729f260ef2e29d477
SumaVarshitha <55625932+SumaVarshitha@users.noreply.github.com>
2020-02-06 17:09:49 +0530

    Update removegitlab.groovy

--- vars/removegitlab.groovy
+++ vars/removegitlab.groovy
@@ -14,27 +14,25 @@ def call(jsondata){
  def jsonString = jsondata
 println(jsonString)
 def jsonObj = readJSON text: jsonString
-println(jsonObj.scm)
+println(jsonObj.scm2)
 
 //String a=jsonObj.alm.projects.project.project_name
-String a=jsonObj.scm.repositories.repository[1].name
-String repoName=a.replaceAll("\\[", "").replaceAll("\\]","");
-String b=jsonObj.scm.repositories.repository[1].branches.branch[0].name1
-String branchName=b.replaceAll("\\[", "").replaceAll("\\]","")
+String a=jsonObj.scm2.projects.project[1].proj_id
+String projectId=a.replaceAll("\\[", "").replaceAll("\\]","");
+
 
 
 
 //sh "rm -rf Text.xml"
  println(a)
- println(repoName)
-   println(branchName)
-sh '''
+println(projectId)
+sh """
 curl -X DELETE \
-  'https://gitlab.com/api/v4/projects/16691253?private_token=VkjgB4Jdbaswh7FNXeC-' \
+  'https://gitlab.com/api/v4/projects/${projectId}?private_token=VkjgB4Jdbaswh7FNXeC-' \
   -H 'accept: application/json' \
   -H 'authorization: Basic c3VtYXZhcnNoaXRoYS5rYW1hdGFtOTk3QGdtYWlsLmNvbTpWa2pnQjRKZGJhc3doN0ZOWGVDLQ==' \
   -H 'cache-control: no-cache' \
   -H 'postman-token: 75bf7f0c-b238-8ced-9af4-9ec6cc4eb8aa'
-  '''
+  """
  
 }
