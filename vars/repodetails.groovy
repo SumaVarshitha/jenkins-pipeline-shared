8b154b1c652b9dc005073501f6c5ef4920ca5f73
SumaVarshitha <55625932+SumaVarshitha@users.noreply.github.com>
2020-02-06 19:26:16 +0530

    Update repodetails.groovy

--- vars/repodetails.groovy
+++ vars/repodetails.groovy
@@ -1,8 +1,18 @@
-def call()
+def call(jsondata)
 {
-sh '''
+  def jsonString = jsondata
+println(jsonString)
+def jsonObj = readJSON text: jsonString
+println(jsonObj.scm)
+
+//String a=jsonObj.alm.projects.project.project_name
+String a=jsonObj.scm.repositories.repository[1].name
+String repoName=a.replaceAll("\\[", "").replaceAll("\\]","");
+  println(a)
+  println(repoName)
+sh """
 curl -X GET \
-  https://api.github.com/repos/SumaVarshitha/Ansible \
+  https://api.github.com/repos/SumaVarshitha/${repoName} \
   -H 'cache-control: no-cache' -o repo.json
-  '''
+  """
   }
