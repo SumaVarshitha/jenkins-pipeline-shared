3f78986f044930204665d3f37d8de85a53888272
SumaVarshitha <55625932+SumaVarshitha@users.noreply.github.com>
2020-02-06 20:16:52 +0530

    Update importgit.groovy

--- vars/importgit.groovy
+++ vars/importgit.groovy
@@ -1,3 +1,3 @@
 def call(){
-sh "curl --request POST --header PRIVATE-TOKEN:VkjgB4Jdbaswh7FNXeC- --data personal_access_token=fe5dc482b82276e3eb836022683f7e6bee389d64&repo_id=232776312&target_namespace=SumaVarshitha/suma https://gitlab.com/api/v4/import/github"
+sh "curl --header PRIVATE-TOKEN:VkjgB4Jdbaswh7FNXeC- --data personal_access_token=fe5dc482b82276e3eb836022683f7e6bee389d64&repo_id=232776312&target_namespace=SumaVarshitha/suma -X POST https://gitlab.com/api/v4/import/github"
 }
