8eb14599ac0dbfd3e3b589d0cb524a5eb6b29f28
SumaVarshitha <55625932+SumaVarshitha@users.noreply.github.com>
2020-02-07 00:41:35 +0530

    Update githubcommit.groovy

--- vars/githubcommit.groovy
+++ vars/githubcommit.groovy
@@ -1,8 +1,24 @@
-def call()
-{
+def call(jsondata){
+ def jsonString = jsondata
+println(jsonString)
+def jsonObj = readJSON text: jsonString
+println(jsonObj.scm)
+
+//String a=jsonObj.alm.projects.project.project_name
+String a=jsonObj.scm.repositories.repository.repo_name
+String repoName=a.replaceAll("\\[", "").replaceAll("\\]","");
+
+env.name = repoName
+
+
+//sh "rm -rf Text.xml"
+ println(a)
+ println(repoName)
+
+
 sh """
 curl -X GET \
-  https://api.github.com/repos/SumaVarshitha/newtask/commits \
+  https://api.github.com/repos/SumaVarshitha/${repoName}/commits \
   -H 'authorization: Basic c3VtYXZhcnNoaXRoYS5rYW1hdGFtOTk3QGdtYWlsLmNvbTpzdW1hc3VqaTI2OA==' \
   -H 'cache-control: no-cache' \
   -H 'postman-token: f302bff6-fe11-93d8-d78d-e756d4a8c5b3'
