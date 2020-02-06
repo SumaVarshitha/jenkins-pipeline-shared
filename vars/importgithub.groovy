fc383d71efa75d7bbdbcaeed2d3410aedc4f5a66
SumaVarshitha <55625932+SumaVarshitha@users.noreply.github.com>
2020-02-06 22:43:53 +0530

    Update importgithub.groovy

--- vars/importgithub.groovy
+++ vars/importgithub.groovy
@@ -17,7 +17,8 @@ curl -X POST \
   -H 'content-type: application/json' \
   -H 'postman-token: b9d72abc-7904-2681-a8ab-39581681eba0' \
   -d '{
-"personal_access_token":"16ec73a2c93c123b9cf370cffe2f0929dd7254e0",
+/*"personal_access_token":"16ec73a2c93c123b9cf370cffe2f0929dd7254e0",*/
+"SumaVarshitha":"sumasuji268"
 "repo_id":${repoId},
 
 "target_namespace":"SumaVarshitha/mani"
