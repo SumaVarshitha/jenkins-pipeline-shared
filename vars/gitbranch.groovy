487a1941315d99e3e09e437bb56fe6415eb37706
SumaVarshitha <55625932+SumaVarshitha@users.noreply.github.com>
2020-02-06 13:22:34 +0530

    Update gitbranch.groovy

--- vars/gitbranch.groovy
+++ vars/gitbranch.groovy
@@ -1,22 +1,29 @@
-/*def call()
-{
-    sh '''
+import groovy.json.*
+
+@NonCPS
+createBranch(String repoName,String branchName){
+
+def jsonSlurper = new JsonSlurper()
+def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/gitcheckout/BranchSha.json"),"UTF-8"))
+def resultJson = jsonSlurper.parse(reader)
+def branchSha = resultJson[0].commit.sha
+    sh """
     curl -X POST \
-  https://api.github.com/repos/SumaVarshitha/newtask/git/refs \
+  https://api.github.com/repos/SumaVarshitha/repoName/git/refs \
   -H 'accept: application/json' \
   -H 'authorization: Basic c3VtYXZhcnNoaXRoYS5rYW1hdGFtOTk3QGdtYWlsLmNvbTpzdW1hc3VqaTI2OA==' \
   -H 'cache-control: no-cache' \
   -H 'content-type: application/json' \
   -H 'postman-token: 8aee7eca-0e7f-361c-6be6-9f3bfbd1f0df' \
   -d '{
-   "ref": "refs/heads/release",
+   "ref": "refs/heads/branchName",
    
-  "sha": "8233111fce0f7fabab8753406eaf72d61b15a7ae"
+  "sha": "branchSha"
 
 
  }'
- '''
-}*/
+ """
+}
 
 def call(jsondata){
  def jsonString = jsondata
@@ -31,23 +38,19 @@ String repoName=a.replaceAll("\\[", "").replaceAll("\\]","");
 String b=jsonObj.scm.repositories.repository[1].branches.branch[0].name1
 String branchName=b.replaceAll("\\[", "").replaceAll("\\]","");
 
-String c=jsonObj.scm.repositories.repository.branches.startPoint
-String branchPath=c.replaceAll("\\[", "").replaceAll("\\]","");
-
-
-
 //sh "rm -rf Text.xml"
  println(a)
  println(repoName)
 println(branchName)
-    println(branchPath)
-httpRequest authentication: 'github', contentType: 'APPLICATION_JSON', customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json']], httpMode: 'POST', requestBody: """
+   
+/*httpRequest authentication: 'github', contentType: 'APPLICATION_JSON', customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json']], httpMode: 'POST', requestBody: """
 {
   
     "ref": "refs/heads/branchName",
    
   "sha": "d2160fec95d3968d0b13ee669b0dbec1837579b1"
    
-}""", responseHandle: 'NONE', url: "https://api.github.com/repos/SumaVarshitha/${repoName}/git/refs"
+}""", responseHandle: 'NONE', url: "https://api.github.com/repos/SumaVarshitha/${repoName}/git/refs"*/
+    createBranch(repoName,branchName)
 }
  
