383072da2c296be130a4d27e70ad5f14c5537eb5
SumaVarshitha <55625932+SumaVarshitha@users.noreply.github.com>
2020-02-06 23:42:18 +0530

    Update commitgitlab.groovy

--- vars/commitgitlab.groovy
+++ vars/commitgitlab.groovy
@@ -1,9 +1,24 @@
-def call()
+import groovy.json.*
+
+@NonCPS
+commitproj()
 {
+
+def jsonSlurper = new JsonSlurper()
+def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/gitcheckout/projectid.json"),"UTF-8"))
+def resultJson = jsonSlurper.parse(reader)
+def projId = resultJson.id
+ 
 sh """
 curl -X GET \
-  'https://gitlab.com/api/v4/projects/15483041/repository/commits?private_token=VkjgB4Jdbaswh7FNXeC-' \
+  'https://gitlab.com/api/v4/projects/${projId}/repository/commits?private_token=VkjgB4Jdbaswh7FNXeC-' \
   -H 'cache-control: no-cache' \
   -H 'postman-token: 2291b4d3-10a3-0e34-20f4-b0e0ccf3b998'
   """
-  }
+
+}
+
+def call()
+{
+commitproj()
+}
