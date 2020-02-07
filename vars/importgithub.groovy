96b217517d1ddcb028f2970b3e34340c4a9ad849
SumaVarshitha <55625932+SumaVarshitha@users.noreply.github.com>
2020-02-07 13:14:13 +0530

    Update importgithub.groovy

--- vars/importgithub.groovy
+++ vars/importgithub.groovy
@@ -26,7 +26,7 @@ curl -X POST \
 }'*/
 sh "curl -X POST 'https://gitlab.com/api/v4/import/github?private_token=VkjgB4Jdbaswh7FNXeC-' -H 'accept: application/json' -H 'authorization: Basic c3VtYXZhcnNoaXRoYS5rYW1hdGFtOTk3QGdtYWlsLmNvbTpWa2pnQjRKZGJhc3doN0ZOWGVDLQ==' -H 'cache-control: no-cache' -H 'content-type: application/json' -H 'postman-token: d663a242-35c7-9e36-7625-802214beba6f' -d '{"personal_access_token":"4107098b755875bb60b0b45504ba0c3d0bc8440e","repo_id":${repoId},"target_namespace":"SumaVarshitha/a"}' "
 //"""
-//}
+}
 
 def call()
 {
