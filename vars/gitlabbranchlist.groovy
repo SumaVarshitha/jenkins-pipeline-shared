4bfdc8f383c19ada7f69f34ba1b7ee6793bbbda2
SumaVarshitha <55625932+SumaVarshitha@users.noreply.github.com>
2020-02-07 12:31:30 +0530

    Update gitlabbranchlist.groovy

--- vars/gitlabbranchlist.groovy
+++ vars/gitlabbranchlist.groovy
@@ -1,10 +1,18 @@
-def call()
+def call(jsondata)
 {
-sh '''
+  def jsonString = jsondata
+println(jsonString)
+def jsonObj = readJSON text: jsonString
+println(jsonObj.scm2)
+
+//String a=jsonObj.alm.projects.project.project_name
+String a=jsonObj.scm2.projects.project[0].proj_id
+String projId=a.replaceAll("\\[", "").replaceAll("\\]","");
+sh """
   curl -X GET \
-  https://gitlab.com/api/v4/projects/16500283/repository/branches \
+  https://gitlab.com/api/v4/projects/${projId}/repository/branches \
   -H 'authorization: Basic c3VtYXZhcnNoaXRoYS5rYW1hdGFtOTk3QGdtYWlsLmNvbTpzdW1hc3VqaTI2OA==' \
   -H 'cache-control: no-cache' \
   -H 'postman-token: 1278cddd-133d-4a82-5588-c065664a4ad7'
-  '''
+  """
 }
