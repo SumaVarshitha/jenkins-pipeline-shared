e7cc6d473cc50122c595bf6b33d381be182bcb52
SumaVarshitha <55625932+SumaVarshitha@users.noreply.github.com>
2020-02-13 11:50:32 +0530

    Update gitrepolist.groovy

--- vars/gitrepolist.groovy
+++ vars/gitrepolist.groovy
@@ -16,6 +16,6 @@ createRepo(request)
 }*/
 def call(){
   
-sh 'curl -v --user SumaVarshitha:sumasuji268 -H "ContentType: application/json; charset=UTF-8"  https://api.github.com/users/SumaVarshitha/repos'
+sh 'curl -v --user SumaVarshitha:sumasuji268 -H "ContentType: application/json; charset=UTF-8"  https://api.github.com/users/SumaVarshitha/repos/json?tree=repos[id,name,owner.login]'
 
 }
