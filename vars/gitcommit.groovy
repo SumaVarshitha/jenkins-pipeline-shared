aa9162565f9124c464551e9e3bc263560c442751
SumaVarshitha <55625932+SumaVarshitha@users.noreply.github.com>
2020-02-24 17:37:01 +0530

    Update gitcommit.groovy

--- vars/gitcommit.groovy
+++ vars/gitcommit.groovy
@@ -22,9 +22,10 @@ print "total no.of collaborators:" +result1
     def count = new String[100]
    for(int j=0;j<result1;j++)
    {
+      println(collab[j])
       sh """
 curl -X GET \
-  'https://api.github.com/repos/SumaVarshitha/game/commits?author=collab[j]' \
+  'https://api.github.com/repos/SumaVarshitha/game/commits?author='${collab[j]}'' \
   -H 'authorization: Basic c3VtYXZhcnNoaXRoYS5rYW1hdGFtOTk3QGdtYWlsLmNvbTpzdW1hc3VqaTI2OA==' \
   -H 'cache-control: no-cache' \
   -H 'postman-token: e89ade00-222a-ac9c-d259-7262600e2883' 
