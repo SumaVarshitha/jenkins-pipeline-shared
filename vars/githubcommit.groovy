95eb9ed0d9d0aaf070c7b6ed92c6d852984d3c8c
SumaVarshitha <55625932+SumaVarshitha@users.noreply.github.com>
2020-02-24 15:11:38 +0530

    Update githubcommit.groovy

--- vars/githubcommit.groovy
+++ vars/githubcommit.groovy
@@ -7,9 +7,9 @@ def jsonSlurper = new JsonSlurper()
 def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/game/commits.json"),"UTF-8"))
 def resultJson = jsonSlurper.parse(reader)
 print "total no.of commits:" +resultJson.commit.size()
-    def jsonSlurper = new JsonSlurper()
-def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/game/collab.json"),"UTF-8"))
-def resultJson1 = jsonSlurper.parse(reader)
+   
+def readera = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/game/collab.json"),"UTF-8"))
+def resultJson1 = jsonSlurper.parse(readera)
 print "total no.of collaborators:" +resultJson1.login.size()
     	
  
