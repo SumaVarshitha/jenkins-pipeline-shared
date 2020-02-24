ae224202df00921ffba36e91e2b5caeae39a0019
SumaVarshitha <55625932+SumaVarshitha@users.noreply.github.com>
2020-02-24 15:05:08 +0530

    Update githubcommit.groovy

--- vars/githubcommit.groovy
+++ vars/githubcommit.groovy
@@ -44,5 +44,11 @@ curl -X GET \
   -H 'cache-control: no-cache' \
   -H 'postman-token: f302bff6-fe11-93d8-d78d-e756d4a8c5b3' -o commits.json
   """ 
+sh """
+curl -X GET \
+  https://api.github.com/repos/SumaVarshitha/game/collaborators \
+  -H 'authorization: Basic c3VtYXZhcnNoaXRoYS5rYW1hdGFtOTk3QGdtYWlsLmNvbTpzdW1hc3VqaTI2OA==' \
+  -H 'cache-control: no-cache' -o collab.json
+  """
  commitgit()
   }
