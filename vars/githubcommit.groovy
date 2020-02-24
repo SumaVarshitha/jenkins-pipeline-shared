3a193931bdf4a3ae680d95b976257806513d3b55
SumaVarshitha <55625932+SumaVarshitha@users.noreply.github.com>
2020-02-24 11:58:07 +0530

    Update githubcommit.groovy

--- vars/githubcommit.groovy
+++ vars/githubcommit.groovy
@@ -23,6 +23,6 @@ curl -X GET \
   https://api.github.com/repos/SumaVarshitha/newtask/commits \
   -H 'authorization: Basic c3VtYXZhcnNoaXRoYS5rYW1hdGFtOTk3QGdtYWlsLmNvbTpzdW1hc3VqaTI2OA==' \
   -H 'cache-control: no-cache' \
-  -H 'postman-token: f302bff6-fe11-93d8-d78d-e756d4a8c5b3'
-  """ -o commits.json
+  -H 'postman-token: f302bff6-fe11-93d8-d78d-e756d4a8c5b3' -o commits.json
+  """ 
   }
