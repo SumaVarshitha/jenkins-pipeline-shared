1e2b2c29bbc07d2107701a107435e6334bfbc67f
SumaVarshitha <55625932+SumaVarshitha@users.noreply.github.com>
2020-02-24 17:53:18 +0530

    Update gitcommit.groovy

--- vars/gitcommit.groovy
+++ vars/gitcommit.groovy
@@ -17,6 +17,13 @@ print "total no.of collaborators:" +result1
    for(int i=0;i<result1;i++){
       collab[i]=resultJson1[i].login
       println(collab[i])
+       sh """
+curl -X GET \
+  'https://api.github.com/repos/SumaVarshitha/game/commits?author='${collab[j]}'' \
+  -H 'authorization: Basic c3VtYXZhcnNoaXRoYS5rYW1hdGFtOTk3QGdtYWlsLmNvbTpzdW1hc3VqaTI2OA==' \
+  -H 'cache-control: no-cache' \
+  -H 'postman-token: e89ade00-222a-ac9c-d259-7262600e2883' 
+  """
 
      
    }
@@ -24,13 +31,7 @@ print "total no.of collaborators:" +result1
    for(int j=0;j<result1;j++)
    {
       
-      sh """
-curl -X GET \
-  'https://api.github.com/repos/SumaVarshitha/game/commits?author='${collab[j]}'' \
-  -H 'authorization: Basic c3VtYXZhcnNoaXRoYS5rYW1hdGFtOTk3QGdtYWlsLmNvbTpzdW1hc3VqaTI2OA==' \
-  -H 'cache-control: no-cache' \
-  -H 'postman-token: e89ade00-222a-ac9c-d259-7262600e2883' 
-  """
+     
       count[j]=0
    for(int i=0;i<result;i++){
       if((resultJson.commit.author.name[i]).equals( collab[j] ))
