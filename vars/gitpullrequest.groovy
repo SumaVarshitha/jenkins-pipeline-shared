3bd73fec2f0c23a6f9569974cf6edf2e1e3f569c
SumaVarshitha <55625932+SumaVarshitha@users.noreply.github.com>
2020-02-25 12:22:10 +0530

    Update gitpullrequest.groovy

--- vars/gitpullrequest.groovy
+++ vars/gitpullrequest.groovy
@@ -1,3 +1,14 @@
+import groovy.json.*
+
+@NonCPS
+pullme(){
+def jsonSlurper = new JsonSlurper()
+def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/game/pull.json"),"UTF-8"))
+def resultJson = jsonSlurper.parse(reader)
+   int result=resultJson.number.size()
+print "total no.of pull requests:" +result
+
+}
 def call()
 {
 sh """
@@ -5,6 +16,7 @@ curl -X GET \
   https://api.github.com/repos/SumaVarshitha/game/pulls \
   -H 'authorization: Basic c3VtYXZhcnNoaXRoYS5rYW1hdGFtOTk3QGdtYWlsLmNvbTpzdW1hc3VqaTI2OA==' \
   -H 'cache-control: no-cache' \
-  -H 'postman-token: edb6f426-ffc4-9070-37fe-2b82bacf0131'
+  -H 'postman-token: edb6f426-ffc4-9070-37fe-2b82bacf0131' -o pull.json
   """
+  pullme()
   }
