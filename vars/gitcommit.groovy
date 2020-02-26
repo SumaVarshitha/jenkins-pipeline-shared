57dcd48aa4ec54466be7b6eba08bc5788eb11848
SumaVarshitha <55625932+SumaVarshitha@users.noreply.github.com>
2020-02-26 15:06:21 +0530

    Update gitcommit.groovy

--- vars/gitcommit.groovy
+++ vars/gitcommit.groovy
@@ -3,11 +3,7 @@ import groovy.json.*
 /*@NonCPS
 commitgit(){
 
-def jsonSlurper = new JsonSlurper()
-def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/game/commits.json"),"UTF-8"))
-def resultJson = jsonSlurper.parse(reader)
-   int result=resultJson.commit.size()
-print "total no.of commits:" +result
+
    println resultJson
    
 def readera = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/game/collab.json"),"UTF-8"))
@@ -66,6 +62,26 @@ curl -X GET \
   -H 'cache-control: no-cache' \
   -H 'postman-token: f302bff6-fe11-93d8-d78d-e756d4a8c5b3' -o commits.json
   """ 
+def jsonSlurper = new JsonSlurper()
+def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/game/commits.json"),"UTF-8"))
+def resultJson = jsonSlurper.parse(reader)
+   int result=resultJson.commit.size()
+print "total no.of commits:" +result
+   
+   int ecount = jsonObj.config.emails.email.size()
+println("No of users "+ ecount)
+ /*  List<String> JSON = new ArrayList<String>();
 
+for(i=0;i<ecount;i++)
+ {
+  for(j=0;j<result;j++)
+  {
+   if(jsonObj.config.emails.email[i]==resultJson[j].author_email)
+   {
+	   JSON.add(JsonOutput.toJson(resultJson[j]))
+   }
+  }
+ }
+   println(JSON)*/
  //commitgit()
   }
