f4ffcbbc554e89e6b20f3499acc4a9b728eab0b5
SumaVarshitha <55625932+SumaVarshitha@users.noreply.github.com>
2020-02-26 12:54:01 +0530

    Update gitpullrequest.groovy

--- vars/gitpullrequest.groovy
+++ vars/gitpullrequest.groovy
@@ -1,23 +1,20 @@
 import groovy.json.*
 
-@NonCPS
-pullme(){
-def jsonSlurper = new JsonSlurper()
-def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/game/pull.json"),"UTF-8"))
-def resultJson = jsonSlurper.parse(reader)
-   int result=resultJson.number.size()
-print "total no.of pull requests:" +result
-   println resultJson
-
-}
-def call()
-{
+def call(jsondata){
+def jsonString = jsondata
+def jsonObj = readJSON text: jsonString
+String a=jsonObj.scm.repositories.repository.repo_name
+String repoName=a.replaceAll("\\[", "").replaceAll("\\]","");
 sh """
 curl -X GET \
-  https://api.github.com/repos/SumaVarshitha/game/pulls \
+  https://api.github.com/repos/SumaVarshitha/${repoName}/pulls \
   -H 'authorization: Basic c3VtYXZhcnNoaXRoYS5rYW1hdGFtOTk3QGdtYWlsLmNvbTpzdW1hc3VqaTI2OA==' \
   -H 'cache-control: no-cache' \
   -H 'postman-token: edb6f426-ffc4-9070-37fe-2b82bacf0131' -o pull.json
   """
-  pullme()
+ def jsonSlurper = new JsonSlurper()
+def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/game/pull.json"),"UTF-8"))
+def resultJson = jsonSlurper.parse(reader)
+   int result=resultJson.number.size()
+print "total no.of pull requests:" +result
   }
