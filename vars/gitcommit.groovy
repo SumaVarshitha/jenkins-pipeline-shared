b78580a45548596e14dc619c89bce6912bb9db13
SumaVarshitha <sumavarshitha.kamatam997@gmail.com>
2020-03-02 16:11:43 +0530

    Update gitcommit.groovy

--- vars/gitcommit.groovy
+++ vars/gitcommit.groovy
@@ -73,7 +73,9 @@ for(i=0;i<ecount;i++)
  }
 	//println(Dispatch)
 	println(JSON)
+	writeFile file: 'output.json', text: "{\"No of Commits\":\"${result}\"}"
 	writeFile file: 'output.json', text: "{\"commit-details\":\"${resultJson}\"" 
+	
 	
 	writeFile file: 'output.json', text: "${JSON}" 
   }
